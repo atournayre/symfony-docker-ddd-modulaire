c97185a5c4cf91b98eb1d45c3d11b58d3aa1269e
atournayre <aurelien.tournayre@gmail.com>
2022-01-08 21:06:37 +0100

    Ajout du script creer_contexte_borne.sh

---
+++ scripts/creer_contexte_borne.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+CONTEXTE_BORNE=$1
+
+SRC=src
+cd $SRC || exit
+
+REP_CONTEXTE_BORNE=$CONTEXTE_BORNE
+REP_APP=$CONTEXTE_BORNE/App
+REP_DOMAINE=$CONTEXTE_BORNE/Domaine
+REP_INFRA=$CONTEXTE_BORNE/Infra
+REP_RESSOURCES=$CONTEXTE_BORNE/Resources
+REP_UI=$CONTEXTE_BORNE/UI
+
+creer_repertoire () {
+  mkdir "$1"
+  cd "$1" || exit
+  touch .gitkeep
+  cd - || exit
+}
+
+creer_repertoire "$REP_CONTEXTE_BORNE"
+creer_repertoire "$REP_APP"
+creer_repertoire "$REP_DOMAINE"
+creer_repertoire "$REP_INFRA"
+creer_repertoire "$REP_RESSOURCES"
+creer_repertoire "$REP_UI"
+
