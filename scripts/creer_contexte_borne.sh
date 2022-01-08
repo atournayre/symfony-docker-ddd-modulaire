3779f551e222a3875f5937aaaf44f6d66dae6b52
atournayre <aurelien.tournayre@gmail.com>
2022-01-08 23:24:50 +0100

    Mise à jour du script de création du contexte borné : templates + génération de fichiers additionnels.

--- scripts/creer_contexte_borne.sh
+++ scripts/creer_contexte_borne.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Variables passées au script
 CONTEXTE_BORNE=$1
@@ -8,12 +8,25 @@ SRC=src
 cd $SRC || exit
 
 # Variables internes
-REP_CONTEXTE_BORNE=$CONTEXTE_BORNE
-REP_APP=$CONTEXTE_BORNE/App
-REP_DOMAINE=$CONTEXTE_BORNE/Domaine
-REP_INFRA=$CONTEXTE_BORNE/Infra
-REP_RESSOURCES=$CONTEXTE_BORNE/Resources
-REP_UI=$CONTEXTE_BORNE/UI
+declare -a repertoires=(
+  "$CONTEXTE_BORNE"
+  "$CONTEXTE_BORNE/App"
+  "$CONTEXTE_BORNE/Domaine"
+  "$CONTEXTE_BORNE/Domaine/DTO"
+  "$CONTEXTE_BORNE/Domaine/Entity"
+  "$CONTEXTE_BORNE/Domaine/Factory"
+  "$CONTEXTE_BORNE/Domaine/Service"
+  "$CONTEXTE_BORNE/Domaine/Traits"
+  "$CONTEXTE_BORNE/Domaine/VO"
+  "$CONTEXTE_BORNE/Infra"
+  "$CONTEXTE_BORNE/Infra/Repository"
+  "$CONTEXTE_BORNE/Infra/Service"
+  "$CONTEXTE_BORNE/Resources"
+  "$CONTEXTE_BORNE/UI"
+  "$CONTEXTE_BORNE/UI/Command"
+  "$CONTEXTE_BORNE/UI/Controller"
+  "$CONTEXTE_BORNE/UI/templates"
+)
 
 # Fonctions
 creer_repertoire () {
@@ -24,20 +37,51 @@ creer_repertoire () {
 }
 
 # Créer les répertoires du contexte borné
-creer_repertoire "$REP_CONTEXTE_BORNE"
-creer_repertoire "$REP_APP"
-creer_repertoire "$REP_DOMAINE"
-creer_repertoire "$REP_INFRA"
-creer_repertoire "$REP_RESSOURCES"
-creer_repertoire "$REP_UI"
-creer_repertoire "$REP_UI/Controller"
-creer_repertoire "$REP_UI/templates"
+arraylength=${#repertoires[@]}
+for (( i=0; i<${arraylength}; i++ ));
+do
+   creer_repertoire "${repertoires[$i]}"
+done
 
-# Créer le fichier services.yaml
-echo "services:" > "$CONTEXTE_BORNE/Resources/services.yaml"
+# App
+# /App
 
-# Créer le fichier routes.yaml
-echo "controllers:" > "$CONTEXTE_BORNE/Resources/routes.yaml"
-echo "    resource: ../src/$CONTEXTE_BORNE/UI/Controller/" >> "$CONTEXTE_BORNE/Resources/routes.yaml"
-echo "    type: annotation" >> "$CONTEXTE_BORNE/Resources/routes.yaml"
+# Domaine
+  # Creer l'entité principale du contexte
+  ENTITE_PRINCIPALE="$CONTEXTE_BORNE/Domaine/Entity/$CONTEXTE_BORNE.php"
+  echo "<?php" > "$ENTITE_PRINCIPALE"
+  echo "declare(strict_types=1);" >> "$ENTITE_PRINCIPALE"
+  echo "" >> "$ENTITE_PRINCIPALE"
+  echo "namespace App\\$CONTEXTE_BORNE\\Domaine\\Entity\\$CONTEXTE_BORNE;" >> "$ENTITE_PRINCIPALE"
+  echo "" >> "$ENTITE_PRINCIPALE"
+  echo "class $CONTEXTE_BORNE" >> "$ENTITE_PRINCIPALE"
+  echo "{" >> "$ENTITE_PRINCIPALE"
+  echo "    // TODO : Implémenter la classe." >> "$ENTITE_PRINCIPALE"
+  echo "}" >> "$ENTITE_PRINCIPALE"
+  rm "$CONTEXTE_BORNE/Domaine/Entity/.gitkeep"
+# /Domaine
+
+# Infra
+# /Infra
+
+# Resources
+  # Créer le fichier services.yaml
+  echo "services:" > "$CONTEXTE_BORNE/Resources/services.yaml"
+
+  # Créer le fichier routes.yaml
+  echo "controllers:" > "$CONTEXTE_BORNE/Resources/routes.yaml"
+  echo "    resource: ../src/$CONTEXTE_BORNE/UI/Controller/" >> "$CONTEXTE_BORNE/Resources/routes.yaml"
+  echo "    type: annotation" >> "$CONTEXTE_BORNE/Resources/routes.yaml"
+# /Resources
+
+# UI
+  # Création du template de base
+  cp ../scripts/templates/templates/base.html.twig "$CONTEXTE_BORNE/UI/templates/base.html.twig"
+  rm "$CONTEXTE_BORNE/UI/templates/.gitkeep"
+
+  # Ajouter un path dans le fichier twig.yaml pour la config du path
+  echo "twig:" >> ../config/packages/twig.yaml
+  echo "  paths:" >> ../config/packages/twig.yaml
+  echo "      '$CONTEXTE_BORNE/UI/templates': '$CONTEXTE_BORNE'" >> ../config/packages/twig.yaml
+# /UI
 
